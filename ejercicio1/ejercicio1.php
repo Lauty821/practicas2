08731a447f2a712751d9edb67ca0062cf9489cab
Lauty821 <LautarodeFCBarcelona@outlook.com>
2023-05-16 00:16:19 -0300

    Commit N°2

---
+++ ejercicio1/ejercicio1.php
@@ -0,0 +1,22 @@
+<?php
+
+class Triangulo
+{
+    private $long1;
+    private $long2;
+    private $long3;
+
+    function __construct($long1, $long2, $long3)
+    {
+        $this->long1=$long1;
+        $this->long2=$long2;
+        $this->long3=$long3;
+    }
+
+    function Calculo()
+    {
+        
+    }
+}
+
+?>
