19a751520b84dc3a774823b09e799b8c1a193ab5
Lauty821 <LautarodeFCBarcelona@outlook.com>
2023-05-19 02:09:02 -0300

    Commit N°5

--- ejercicio1-1.php
+++ ejercicio1-1.php
@@ -13,9 +13,19 @@ class Triangulo
         $this->long3=$long3;
     }
 
-    function Calculo()
+    function Calculo($long1, $long2, $long3)
     {
-        
+        $long1exacta=$this->long1*2;
+        return $long1exacta;
+
+        $long3exacta=$this->$long3*2;
+        return $long3exacta;
+
+        $prelong2=$this->$long3exacta-$this->$long1exacta;
+        return $prelong2;
+
+        $long2exacta=$this->$prelong2/2;
+        return $long2exacta;
     }
 }
 
